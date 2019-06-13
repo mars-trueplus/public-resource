680eed2c84137d7c148ce3be9201898856c80f6d
XMars <mars@trueplus.vn>
2019-06-13 16:14:03 +0700

    Update install_magento.sh

--- install_magento.sh
+++ install_magento.sh
@@ -16,7 +16,7 @@ SOURCE_FOLDER="/home/pos/src"
 sudo apt-get update && sudo apt-get upgrade -y && sudo apt-get install apache2 mysql-server -y
 sudo usermod -aG www-data $USER
 sudo apt-get -y update
-sudo add-apt-repository ppa:ondrej/php
+sudo add-apt-repository ppa:ondrej/php -y
 sudo apt-get -y update
 sudo apt-get install -y php7.1 libapache2-mod-php7.1 php7.1-common php7.1-gd php7.1-mysql php7.1-curl php7.1-intl php7.1-xsl php7.1-mbstring php7.1-zip php7.1-bcmath php7.1-iconv php7.1-soap
 sudo mysql -u root -p -e "create user 'pos'@'localhost' identified by 'pos'"
