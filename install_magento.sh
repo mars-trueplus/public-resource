648170c43a82e0f6c347c1a72a6be0754300843f
XMars <mars@trueplus.vn>
2019-06-17 13:42:12 +0700

    Update install_magento.sh

--- install_magento.sh
+++ install_magento.sh
@@ -18,7 +18,7 @@ sudo usermod -aG www-data $USER
 sudo apt-get -y update
 sudo add-apt-repository ppa:ondrej/php -y
 sudo apt-get -y update
-sudo apt-get install -y php7.1 libapache2-mod-php7.1 php7.1-common php7.1-gd php7.1-mysql php7.1-curl php7.1-intl php7.1-xsl php7.1-mbstring php7.1-zip php7.1-bcmath php7.1-iconv php7.1-soap
+sudo apt-get install -y php7.1 libapache2-mod-php7.1 php7.1-common php7.1-gd php7.1-mysql php7.1-mcrypt php7.1-curl php7.1-intl php7.1-xsl php7.1-mbstring php7.1-zip php7.1-bcmath php7.1-iconv php7.1-soap
 sudo mysql -u root -p -e "create user 'pos'@'localhost' identified by 'pos'"
 sudo mysql -u root -p -e "grant all privileges on *.* to 'pos'@'localhost' identified by 'pos'"
 mysql -u pos -ppos -e "create database pos"
